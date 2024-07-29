99dda8ed6705875834f46ba8fa3cbee6232a5aee
Andrey Nemerov <andrey.nemerov@rt.ru>
2024-07-29 09:50:24 +0300

    resnet

--- model/resnet.py
+++ model/resnet.py
@@ -11,7 +11,7 @@ class Bottleneck(nn.Module):
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)     
-        self.conv3 = nn.Conv2d(planes, planes * self.expansion)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, stride=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * self.expansion)
         self.downsample = downsample
         
